be3fb2fa6062c89c5168094a7994035d36000e27
Thiago Volpi Rabethge~ <thiago.rabethge@gmail.com>
2024-09-29 19:10:13 -0300

    fix cors

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+cors = CORS(app, origins=['http://localhost:5173', 'https://example.com'])
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
